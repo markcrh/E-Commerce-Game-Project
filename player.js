f63ea6676ddcbfa5c5e4fa5b1c2cc5d58e3b2ee5
mark <markcrh97@gmail.com>
2024-09-30 11:40:45 +0100

    main layout structure, working player class and movement

---
+++ player.js
@@ -0,0 +1,12 @@
+class Character {
+    constructor(hp, posX, posY, dirX, dirY, width, height, speed){
+        this.hp = hp;
+        this.posX = posX;
+        this.posY = posY;
+        this.dirX = dirX;
+        this.dirY = dirY;
+        this.width = 50;
+        this.height = 50;
+        this.speed = speed;
+    }
+}
