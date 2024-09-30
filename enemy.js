6ed7422efa4f04d107e076cfa70a10f3d49ac018
mark <markcrh97@gmail.com>
2024-09-30 13:08:09 +0100

    working enemy movement (randomizer)

--- enemy.js
+++ enemy.js
@@ -4,12 +4,14 @@ class Enemy {
     this.posX = posX;
     this.posY = posY;
     this.dirX = 1;
-    this.dirY = 1;
+    this.dirY = 0;
     this.width = 50;
     this.height = 50;
     this.speed = 3;
     this.sprite = document.createElement("div");
-    this.interval = setInterval(this.moveX.bind(this), 10);
+    this.intervalX = setInterval(this.moveX.bind(this), 10);
+    this.intervalY = setInterval(this.moveY.bind(this), 10);
+
   }
 
   insert() {
@@ -27,7 +29,18 @@ class Enemy {
     if (newX >= 0 && newX <= 1300 - this.width) {
       this.posX = newX;
       this.sprite.style.left = this.posX + "px";
+    } else {
+      let random = Math.floor(Math.random()*100)
+      console.log(random)
+      if (random % 2 == 0){
+        this.dirY = 1;
+        this.dirX = 0;
+      } else {
+        this.dirY = -1;
+        this.dirX = 0;
+      }
     }
+  
   }
   moveY() {
     let newY = this.posY + this.speed * this.dirY;
@@ -35,6 +48,15 @@ class Enemy {
     if (newY >= 0 && newY <= 935 - this.height) {
       this.posY = newY;
       this.sprite.style.top = this.posY + "px";
+    } else {
+      let random = Math.floor(Math.random()*100);
+      if (random % 2 == 0) {
+        this.dirX = -1;
+        this.dirY = 0;
+      } else {
+        this.dirX = 1;
+        this.dirY = 0;
+      }
     }
   }
 }
